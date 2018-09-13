a5cbda6faf729dbcfdb71a64ba123639106591ac
Kazi Khaled Saif <kksaif05@hotmail.com>
2018-09-14 01:48:41 +0600

    FireBase

--- app/src/main/java/com/kazikhaledsaif/tripadvisor/LogInActivity.java
+++ app/src/main/java/com/kazikhaledsaif/tripadvisor/LogInActivity.java
@@ -5,12 +5,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class LogInActivity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+        mAuth = FirebaseAuth.getInstance();
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
     }
 
     public void createNewAccount(View view) {
--- app/src/main/java/com/kazikhaledsaif/tripadvisor/SignUpActivity.java
+++ app/src/main/java/com/kazikhaledsaif/tripadvisor/SignUpActivity.java
@@ -1,26 +1,38 @@
 package com.kazikhaledsaif.tripadvisor;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignUpActivity extends AppCompatActivity {
 
     private EditText mFullNameET,mUserNameET,mPasswordET,mAddressET,mPhoneNumbET,mEmailET;
-
+    private String TAG = "Firebase";
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-
+        mAuth = FirebaseAuth.getInstance();
         mFullNameET = findViewById(R.id.signUpFullNameET);
         mUserNameET = findViewById(R.id.signUpUserNameET);
         mPasswordET = findViewById(R.id.signUpPasswordET);
         mAddressET = findViewById(R.id.signUpAddressET);
         mPhoneNumbET = findViewById(R.id.signUpPhoneNumET);
         mEmailET = findViewById(R.id.signUpEmailET);
+
 
     }
 
@@ -39,5 +51,38 @@ public class SignUpActivity extends AppCompatActivity {
         String address= mAddressET.getText().toString();
         String email= mEmailET.getText().toString();
         String phoneNum= mPhoneNumbET.getText().toString();
+
+        if(TextUtils.isEmpty(email)){
+            mEmailET.setError("Email is Empty");
+            return;
+        }
+        if(TextUtils.isEmpty(password)){
+
+            mPasswordET.setError("Password is Empty");
+            return;
+        }
+
+        else {
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d(TAG, "createUserWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                Toast.makeText(SignUpActivity.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+
+                            }
+
+                            // ...
+                        }
+                    });
+        }
     }
 }
