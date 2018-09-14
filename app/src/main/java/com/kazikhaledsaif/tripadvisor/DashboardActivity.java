2061a6f6b7a658925c214809abc94785790b0260
parvez6084 <parvez.ka19@gmail.com>
2018-09-15 02:45:53 +0600

    parvez 2nd time edit

--- app/src/main/java/com/kazikhaledsaif/tripadvisor/DashboardActivity.java
+++ app/src/main/java/com/kazikhaledsaif/tripadvisor/DashboardActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -11,11 +13,24 @@ import com.google.firebase.auth.FirebaseUser;
 public class DashboardActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    ViewFlipper viewFlipper;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
         mAuth = FirebaseAuth.getInstance();
+
+        int images[] ={ R.drawable.bich, R.drawable.hotel, R.drawable.hilli, R.drawable.del, R.drawable.wallhaven};
+        viewFlipper =findViewById(R.id.backgroundimgViewer);
+
+        for (int image: images){
+            imgSlider(image);
+        }
+
+
 
     }
     @Override
@@ -37,4 +52,25 @@ public class DashboardActivity extends AppCompatActivity {
         finish();
         startActivity(intent);
     }
+
+
+    public void imgSlider(int image){
+
+
+        ImageView imageView = new ImageView(this);
+        imageView.setBackgroundResource(image);
+
+        viewFlipper.addView(imageView);
+        viewFlipper.setFlipInterval(4000);
+
+        viewFlipper.setAutoStart(true);
+        viewFlipper.setInAnimation(this,android.R.anim.slide_in_left);
+        viewFlipper.setInAnimation(this,android.R.anim.slide_out_right);
+
+    }
+
+
+
+
+
 }
