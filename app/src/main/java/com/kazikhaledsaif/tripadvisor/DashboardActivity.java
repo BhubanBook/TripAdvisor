97f53e12a5d9f80d7cee197cf63d0b4b4ed5e220
Kazi Khaled Saif <kksaif05@hotmail.com>
2018-09-15 03:48:19 +0600

    Saif edited 3.48 am

--- app/src/main/java/com/kazikhaledsaif/tripadvisor/DashboardActivity.java
+++ app/src/main/java/com/kazikhaledsaif/tripadvisor/DashboardActivity.java
@@ -1,10 +1,14 @@
 package com.kazikhaledsaif.tripadvisor;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ViewFlipper;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -14,8 +18,8 @@ public class DashboardActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     ViewFlipper viewFlipper;
-
-
+    LinearLayout mNearBy;
+    Dialog mDialog ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +27,9 @@ public class DashboardActivity extends AppCompatActivity {
         setContentView(R.layout.activity_dashboard);
         mAuth = FirebaseAuth.getInstance();
 
+        mNearBy = findViewById(R.id.nearbyBTN);
+
+        mDialog=new Dialog(this);
         int images[] ={ R.drawable.bich, R.drawable.hotel, R.drawable.hilli, R.drawable.del, R.drawable.wallhaven};
         viewFlipper =findViewById(R.id.backgroundimgViewer);
 
@@ -30,6 +37,16 @@ public class DashboardActivity extends AppCompatActivity {
             imgSlider(image);
         }
 
+        mNearBy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            /*    Intent intent = new Intent(DashboardActivity.this, NearbyActivity.class);
+                finish();
+                startActivity(intent);*/
+
+                dialog_Messenger();
+            }
+        });
 
 
     }
@@ -69,7 +86,15 @@ public class DashboardActivity extends AppCompatActivity {
 
     }
 
+    public void dialog_Messenger(){
 
+
+        mDialog.setContentView(R.layout.dialogbox);
+
+
+        mDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        mDialog.show();
+    }
 
 
 
